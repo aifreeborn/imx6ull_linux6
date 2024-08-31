4fe96aa3fbb6bc889a2a3b57d8747f56c252ede1
aifreeborn <takeno2020@163.com>
2024-08-31 22:17:52 +0800

    BSP:add 100ask board based on imx_v7_defconfig and imx6ull-14x14-evk-emmc.dts

--- aibuild.sh
+++ aibuild.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+make mrproper
+# make imx_v7_defconfig
+make imx_100ask_emmc_defconfig
+make zImage -j4
+echo "############################### build zImage end #######################."
+make dtbs
+echo "############################### build dtbs end #######################."
+
+# 将生成的zImage和imx6ull-100ask-emmc.dtb拷贝到SD卡的boot分区中
+
+# sudo dd if=arch/arm/boot/zImage of=/dev/sdb bs=512 seek=2048 conv=fsync && sync
+# sudo dd if=arch/arm/boot/dts/imx6ull-100ask-emmc.dtb of=/dev/sdb bs=512 seek=20480 conv=fsync && sync
+
